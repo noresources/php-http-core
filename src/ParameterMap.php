4c76faee7e6d67326b8e506740bf6717cd2ff283
Renaud Guillard <dev@nore.fr>
2021-06-06 17:05:24 +0200

    v1.2.0 ParameterMapInterface & package dependencies update
    * ParameterMapInterface specification
      * Interface now must explicitely implements PSR ContainerInterface
      * Add specifications on implementation behavior
    * Update ns-php-core to v2.4
      * Adjust code to latest ns-php-core (CaseInsensitiveKeyMapTrait)

--- src/ParameterMap.php
+++ src/ParameterMap.php
@@ -13,6 +13,8 @@ namespace NoreSources\Http;
 use Psr\Container\ContainerInterface;
 use NoreSources\Container;
 use NoreSources\Container\CaseInsensitiveKeyMapTrait;
+use NoreSources\Container\ArrayAccessContainerInterfaceTrait;
+use NoreSources\Type\TypeDescription;
 
 /**
  * Case-insensitive key-value parameter map.
@@ -22,12 +24,29 @@ use NoreSources\Container\CaseInsensitiveKeyMapTrait;
  * @see https://tools.ietf.org/html/rfc7231#section-3.1.1.1
  * @see https://tools.ietf.org/html/rfc4288#section-4.3
  */
-class ParameterMap implements ParameterMapInterface, ContainerInterface
+class ParameterMap implements ParameterMapInterface
 {
 
 	use CaseInsensitiveKeyMapTrait;
+	use ArrayAccessContainerInterfaceTrait;
 
-	protected function onKeyNotFound($key)
+	public function offsetGet($name)
+	{
+		if (!$this->offsetExists($name))
+			return NULL;
+		return $this->caselessOffsetGet($name);
+	}
+
+	public function offsetSet($name, $value)
+	{
+		if (!\is_string($name))
+			throw new \InvalidArgumentException(
+				'Invalid parameter name. String expected, got ' .
+				TypeDescription::getName($name));
+		$this->caselessOffsetSet($name, $value);
+	}
+
+	protected function newNotFoundException($key)
 	{
 		throw new ParameterNotFoundException($key);
 	}
--- src/ParameterMapInterface.php
+++ src/ParameterMapInterface.php
@@ -10,7 +10,23 @@
  */
 namespace NoreSources\Http;
 
+use Psr\Container\ContainerInterface;
+
+/**
+ * Key-value map for several HTTP message header value.
+ *
+ * <ul>
+ * <li>Key must be a token string as described in RFC 7231</li>
+ * <li>Value must be a token or quoted-value string as described in RRC 7231</li>
+ * <li>Keys are case-insensitive</li>
+ * <li>offsetGet($key) MUST return NULL when parameter cannot be found</li>
+ * <li>offsetSet($key, $value) MUST throw InvalidArgumentException if the key is not a string</li>
+ * <li>offsetSet($key, $value) MAY validate $key and $value and throw
+ * InvalidArgumentException</li>
+ * <li>get() MUST throw ParameterNotFoundException on non-existing key</li>
+ * </ul>
+ */
 interface ParameterMapInterface extends \ArrayAccess, \Countable,
-	\IteratorAggregate
+	\IteratorAggregate, ContainerInterface
 {
 }
